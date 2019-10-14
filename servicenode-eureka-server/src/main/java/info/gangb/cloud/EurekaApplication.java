4b9801a2e8c9061dedd95b2cb5a67c21f103d229
jianggang <gangb.info@gmail.com>
2019-10-14 15:29:55 +0800

    add Eureka server

---
+++ servicenode-eureka-server/src/main/java/info/gangb/cloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package info.gangb.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jianggang
+ * @date 2019/10/12 15:50:29
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
